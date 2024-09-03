d8b239ce269ac490793f32e6ca9426df40be39fc
FauziXR7 <162082115+FauziXR7@users.noreply.github.com>
2024-09-03 09:16:47 +0700

    Add files via upload

---
+++ lib/stack_position.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class StackPosition extends StatelessWidget {
+  const StackPosition({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              height: 200,
+              child: Stack(
+                children: [
+                  Positioned(
+                    child: Image(
+                      image: AssetImage("asset/arle.png"),
+                    ),
+                    width: MediaQuery.of(context).size.width * 35 / 100,
+                  ),
+                  Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.only(left: 10, right: 10),
+                      child: Row(
+                        children: [Text("Rarw"), Icon(Icons.woman_2)],
+                      )),
+                ],
+              ),
+              // child: Column(
+              //   children: [
+              //     Image(
+              //       image: AssetImage("asset/Arle.png"),
+              //       width: MediaQuery.of(context).size.width * 35 / 100,
+              //     ),
+              //     Container(
+              //         width: MediaQuery.of(context).size.width,
+              //         padding: EdgeInsets.only(left: 10, right: 10),
+              //         child: Row(
+              //           children: [Text("Bojo Ku"), Icon(Icons.woman_2)],
+              //         )),
+              //   ],
+              // ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
